9860f531425425efca7470be342f24880146e71a
Adam Coyne <adam.coyne@aerialsphere.com>
2020-08-13 20:07:48 -0500

    Add onClick/getPositionInfo demo

---
+++ 03-onclick-getpositioninfo/demo.js
@@ -0,0 +1,28 @@
+const elementId = "as1";
+const data = {layers: []};
+const aerialSphere = new AerialSphere(document.getElementById(elementId), data);
+
+aerialSphere.onClick = function () {
+    console.log("onClick")
+    this.getVar("mouse.x", function(mouseX) {
+        this.mouseX = mouseX;
+        this.getVar("mouse.y", function(mouseY) {
+            this.mouseY = mouseY;
+            this.getPositionInfo(this.mouseX, this.mouseY, function() {
+                const lat = arguments[0];
+                const lon = arguments[1];
+                const pan = arguments[2];
+                const tilt = arguments[3];
+
+                const message = "Clicked at " + this.mouseX + ", " + this.mouseY + "\n"
+                    + "Latitude: " + lat + "\n"
+                    + "Longitude: " + lon + "\n"
+                    + "Pan: " + pan + "\n"
+                    + "Tilt: " + tilt + "\n";
+
+                console.log(message);
+                alert(message);
+            });
+        });
+    });
+};
